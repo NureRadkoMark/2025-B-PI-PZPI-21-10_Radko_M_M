fa1df1b8b5708a695c8a6a44c3c0d893c021fee4
Mark Radko <mark.radko@nure.ua>
2025-06-13 19:50:59 +0300

    Add files via upload

---
+++ Park4Flow/WebClient/src/utils/consts.js
@@ -0,0 +1,42 @@
+export const REGISTER_ROUTE = '/register'
+export const LOGIN_ROUTE = '/login'
+
+export const PROFILE_ROUTE = '/profile'
+
+export const ADD_VEHICLE_ROUTE = '/add-vehicle'
+
+export const EDIT_VEHICLE_ROUTE = '/edit-vehicle'
+
+export const PASSWORD_RECOVERY_ROUTE = '/pass-recovery'
+
+export const MAP_ROUTE = '/map'
+
+export const FAVOURITE_PARKINGS_PAGE = '/favorite-parkings'
+
+export const SUBSCRIPTION_PAGE = '/subscription-page'
+
+export const USER_MANAGEMENT_PAGE = '/user-manage'
+
+export const TARIFF_PLANS_MANAGEMENT_PAGE = '/tariff-plans'
+
+export const ADD_TARIFF_PLAN = '/create-tariff-plan'
+
+export const EDIT_TARIFF_PLAN = '/edit-tariff-plan'
+
+export const BACKUP_ROUTE = '/backup'
+
+export const USER_RESERVATIONS_ROUTE = '/bookings'
+
+export const PAYMENT_RESULT_ROUTE = '/payment-result'
+
+export const PARKING_REGISTER_PAGE = '/register-parking'
+
+export const CREATE_RESERVATION_MODAL_PAGE = '/reservation-create'
+
+export const OWNER_PARKINGS_PAGE = '/my-parkings'
+
+export const PARKING_STATISTICS_PAGE = '/parking-statistics'
+
+export const PARKING_MANAGE_PAGE = '/parking-manage'
+
+export const HOME_ROUTE = '/home'
